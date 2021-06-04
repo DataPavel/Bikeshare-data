b0e9a35296a668aaee891b75cc933c9f8e9a3e4a
Pavel-Averin <averinjr@gmail.com>
2021-06-04 18:33:46 +0300

    Add main function

--- bikeshare.py
+++ bikeshare.py
@@ -6,3 +6,15 @@
 CITY_DATA = { 'chicago': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\chicago.csv',
               'new york': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\new_york_city.csv',
               'washington': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\washington.csv' }
+
+
+# main function collects all the functions in the project
+def main():
+    while True:
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+    main()
