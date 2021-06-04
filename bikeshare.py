99aec9de28c35435acf7b35e2f40e0531e2b6d96
Pavel-Averin <averinjr@gmail.com>
2021-06-04 19:23:55 +0300

    Load and filtering data

--- bikeshare.py
+++ bikeshare.py
@@ -29,6 +29,35 @@ def get_filters():
 
     return city, month, day
 
+def load_data(city, month, day):
+ #   """
+ #  Loads data for the specified city and filters by month and day if applicable.
+
+ # Args:
+ #    (str) city - name of the city to analyze
+ #   (str) month - name of the month to filter by, or "all" to apply no month filter
+ #  (str) day - name of the day of week to filter by, or "all" to apply no day filter
+ #Returns:
+ #   df - pandas DataFrame containing city data filtered by month and day
+ # """
+
+    df = pd.read_csv(CITY_DATA[city])
+
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+# Adding new columns to dataframe "month" and "day_of_week"
+    df['month'] =df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day_name()
+#filtering by month
+    if month != 'all':
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = int(months.index(month)) + 1
+        df = df[df['month'] == month]
+#filtering by day
+    if day != 'all':
+        df = df[df['day_of_week'] == day.title()]
+    print('-' * 40)
+
+    return df
 
 
 # main function collects all the functions in the project
