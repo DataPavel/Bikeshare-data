df0513a69ca704ac69664372fa3b6058aaaf8321
Pavel-Averin <averinjr@gmail.com>
2021-06-04 18:41:13 +0300

    Add filters by city, day and month

--- bikeshare.py
+++ bikeshare.py
@@ -7,10 +7,34 @@ CITY_DATA = { 'chicago': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikesh
               'new york': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\new_york_city.csv',
               'washington': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\washington.csv' }
 
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # get user input for city (chicago, new york city, washington).
+    city = input('What city do you want to explore Washington, New York or Chicago?').lower()
+
+    # get user input for month (all, january, february, ... , june)
+    month = input('What month do you want to select? January, February, March, April, May, June or all? ').lower()
+    # get user input for day of week (all, monday, tuesday, ... sunday)
+    day = input('What day of the week do you want to select? Monday, Tuesday, Wednesday, Thursday, '
+                'Friday, Saturday, Sunday or all? ').lower()
+
+
+    return city, month, day
+
+
 
 # main function collects all the functions in the project
 def main():
     while True:
+        city, month, day = get_filters() 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
