ccc9c0bbf0baab808d80c1397aae6abf7dbf0059
Pavel-Averin <averinjr@gmail.com>
2021-06-04 18:28:37 +0300

    Add dictionary of csv files

---
+++ bikeshare.py
@@ -0,0 +1,8 @@
+
+
+
+
+# add dictionary of csv files
+CITY_DATA = { 'chicago': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\chicago.csv',
+              'new york': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\new_york_city.csv',
+              'washington': 'C:\\Users\\averi\\udacity-git-course\\Projects\\bikeshare\\washington.csv' }
